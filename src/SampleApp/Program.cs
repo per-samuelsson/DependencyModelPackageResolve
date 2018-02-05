c3165fb3ca480d37e0ed1b54f0c7f7af71456b8c
Per Samuelsson <per@starcounter.com>
2018-02-05 10:41:34 +0100

    Sample application and target to illustrate package resolving using composite resolver.

---
+++ src/SampleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.Extensions.DependencyModel;
+using Microsoft.Extensions.DependencyModel.Resolution;
+
+namespace SampleApp {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var dir = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+            var assemblyPath = Path.Combine(dir, args[0]);
+
+            var assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+
+            var dependencyContext = DependencyContext.Load(assembly);
+            
+            var assemblyResolver = new CompositeCompilationAssemblyResolver(
+                new ICompilationAssemblyResolver[] {
+                    new AppBaseCompilationAssemblyResolver(Path.GetDirectoryName(assemblyPath)),
+                    new ReferenceAssemblyPathResolver(),
+                    new PackageCompilationAssemblyResolver()
+            });
+
+            var name = new AssemblyName("Ninject, Version=3.3.0.0, Culture=neutral, PublicKeyToken=c7192dc5380945e7");
+            var runtimeLibrary = dependencyContext.RuntimeLibraries.First(
+                (rl) => string.Equals(rl.Name, name.Name, StringComparison.OrdinalIgnoreCase));
+            
+            var compilationLibrary = new CompilationLibrary(
+                runtimeLibrary.Type,
+                runtimeLibrary.Name,
+                runtimeLibrary.Version,
+                runtimeLibrary.Hash,
+                runtimeLibrary.RuntimeAssemblyGroups.SelectMany(g => g.AssetPaths),
+                runtimeLibrary.Dependencies,
+                runtimeLibrary.Serviceable
+            );
+            
+            var assemblies = new List<string>();
+            assemblyResolver.TryResolveAssemblyPaths(compilationLibrary, assemblies);
+            Console.Write(assemblies.Count);  // 0 / zero when executed from published context
+        }
+    }
+}
---
+++ src/Target/TestModule.cs
@@ -0,0 +1,13 @@
+﻿
+using Ninject.Modules;
+using System;
+
+namespace Target {
+  
+    // Force reference to be emitted
+    public class TestModule : NinjectModule {
+        public override void Load() {
+            throw new NotImplementedException();
+        }
+    }
+}
